0cb660a4db308d0b71f1840d4c17f6e353648927
Badr9907 <awlhajb@gmail.com>
2025-07-09 12:26:24 +0100

    Update main.go

--- main.go
+++ main.go
@@ -13,5 +13,11 @@ func main() {
 		fmt.Println("number of arguments is wrong!")
 		return
 	}
+	for _, v := range os.Args[1] {
+		if v < 32 || v >= 127 {
+			fmt.Println("error, string is not valid ")
+			return
+		}
+	}
 	function.AppendArt(strings.Split(os.Args[1], "\\n"), function.AsciiArtTable())
 }
