2fb95934bc334b0dcebd5d581c2b40ec119d3342
Badr9907 <awlhajb@gmail.com>
2025-07-09 12:26:59 +0100

    Update asciiArtTable.go

--- functions/asciiArtTable.go
+++ functions/asciiArtTable.go
@@ -5,7 +5,7 @@ import (
 	"os"
 	"strings"
 )
-
+// a function to read the file and store the ascii values in a map  
 func AsciiArtTable() map[int]string {
 	fileData, err := os.ReadFile("standard.txt")
 	if err != nil {
@@ -13,9 +13,11 @@ func AsciiArtTable() map[int]string {
 		return map[int]string{}
 	}
 	blocks := strings.Split(string(fileData), "\n\n")
+	// after split formatting if there is an extra new line
 	if strings.Count(blocks[0], "\n") == 8 {
 		blocks[0] = blocks[0][1:]
 	}
+	//initializing a map and storing the ascii values in
 	asciiArtTable := make(map[int]string)
 	key := 32
 	for _, block := range blocks {
